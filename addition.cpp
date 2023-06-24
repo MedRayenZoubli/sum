78c5f45f9738af8ec3b0f783a25e4e4999b871a8
rayen zoubli <91822359+MedRayenZoubli@users.noreply.github.com>
2023-06-24 19:15:25 +0200

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "a=" << endl;
+    cin >> a;
+    cout << "b=" << endl;
+    cin >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+}
